e5e7485cff62240d5e158571292620cdcc7a85fc
dgusfr <diegogustavo.fr@gmail.com>
2024-11-10 11:10:18 -0300

    update

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
-from controllers.biblioteca_controller import BibliotecaController
+from menu import menu
 
-def exibir_menu():
-    print("1. Adicionar Livro")
-    print("2. Adicionar Usuário")
-    print("3. Realizar Empréstimo")
-    print("4. Devolver Livro")
-    print("5. Sair")
-
-def menu_principal():
-    while True:
-        exibir_menu()
-        opcao = input("Escolha uma opção: ")
-        if opcao == '1':
-            pass
-        elif opcao == '2':
-            pass
-        elif opcao == '3':
-            pass
-        elif opcao == '4':
-            pass
-        elif opcao == '5':
-            print("Encerrando o sistema...")
-            break
+if __name__ == "__main__":
+    print("Bem-vindo ao Sistema de Biblioteca")
+    menu()
