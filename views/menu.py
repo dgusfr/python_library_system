9e4e77176c631df9bfdd2fc753b18e0ecd5923c7
dgusfr <diegogustavo.fr@gmail.com>
2025-08-11 19:57:34 -0300

    feat add

--- views/menu.py
+++ views/menu.py
@@ -23,49 +23,41 @@ def menu():
         exibir_menu()
         opcao = input("Escolha uma opção: ")
 
-        if opcao == "1":
-            titulo = input("Digite o título do livro: ")
-            autor = input("Digite o autor do livro: ")
-            livro = Livro(titulo, autor)
-            biblioteca.adicionar_livro(livro)
-            print("Livro adicionado com sucesso.")
-
-        elif opcao == "2":
-            biblioteca.listar_livros()
-
-        elif opcao == "3":
-            titulo = input("Digite o título do livro a ser removido: ")
-            biblioteca.remover_livro(titulo)
-
-        elif opcao == "4":
-            nome = input("Digite o nome do usuário: ")
-            idade = input("Digite a idade do usuário: ")
-            usuario = Usuario(nome, idade)
-            biblioteca.adicionar_usuario(usuario)
-            print("Usuário adicionado com sucesso.")
-
-        elif opcao == "5":
-            biblioteca.listar_usuarios()
-
-        elif opcao == "6":
-            nome = input("Digite o nome do usuário a ser removido: ")
-            biblioteca.remover_usuario(nome)
-
-        elif opcao == "7":
-            titulo = input("Digite o título do livro para empréstimo: ")
-            nome = input("Digite o nome do usuário que fará o empréstimo: ")
-            biblioteca.realizar_emprestimo(titulo, nome)
-
-        elif opcao == "8":
-            titulo = input("Digite o título do livro a ser devolvido: ")
-            biblioteca.registrar_devolucao(titulo)
-
-        elif opcao == "9":
-            print("Saindo do sistema...")
-            break
-
-        else:
-            print("Opção inválida. Tente novamente.")
+        match opcao:
+            case "1":
+                titulo = input("Digite o título do livro: ")
+                autor = input("Digite o autor do livro: ")
+                livro = Livro(titulo, autor)
+                biblioteca.adicionar_livro(livro)
+                print("Livro adicionado com sucesso.")
+            case "2":
+                biblioteca.listar_livros()
+            case "3":
+                titulo = input("Digite o título do livro a ser removido: ")
+                biblioteca.remover_livro(titulo)
+            case "4":
+                nome = input("Digite o nome do usuário: ")
+                idade = input("Digite a idade do usuário: ")
+                usuario = Usuario(nome, idade)
+                biblioteca.adicionar_usuario(usuario)
+                print("Usuário adicionado com sucesso.")
+            case "5":
+                biblioteca.listar_usuarios()
+            case "6":
+                nome = input("Digite o nome do usuário a ser removido: ")
+                biblioteca.remover_usuario(nome)
+            case "7":
+                titulo = input("Digite o título do livro para empréstimo: ")
+                nome = input("Digite o nome do usuário que fará o empréstimo: ")
+                biblioteca.realizar_emprestimo(titulo, nome)
+            case "8":
+                titulo = input("Digite o título do livro a ser devolvido: ")
+                biblioteca.registrar_devolucao(titulo)
+            case "9":
+                print("Saindo do sistema...")
+                break
+            case _:
+                print("Opção inválida. Tente novamente.")
 
 
 if __name__ == "__main__":
