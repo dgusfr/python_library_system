454f712bea1cd142be4e749e923322b0fa4c387e
dgusfr <diegogustavo.fr@gmail.com>
2025-08-11 20:05:01 -0300

    feat add

--- models/biblioteca.py
+++ models/biblioteca.py
@@ -10,8 +10,11 @@ class Biblioteca:
         self.usuarios.append(usuario)
 
     def listar_livros(self):
-        if not self.livros:
-            print("Nenhum livro cadastrado.")
-        else:
-            for livro in self.livros:
-                print(livro)
+        try:
+            if not self.livros:
+                print("Nenhum livro cadastrado.")
+            else:
+                for livro in self.livros:
+                    print(livro)
+        except Exception as e:
+            print(f"Erro ao listar livros: {e}")
