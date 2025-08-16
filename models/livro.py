4fd7bf296dc2d6b0c55f8ef09e6dd1dd398b11f1
dgusfr <diegogustavo.fr@gmail.com>
2025-08-16 13:25:06 -0300

    feat add

--- models/livro.py
+++
@@ -1,9 +0,0 @@
-class Livro:
-    def __init__(self, titulo, autor, ano):
-        self.titulo = titulo
-        self.autor = autor
-        self.ano = ano
-        self.disponivel = True
-
-    def __str__(self):
-        return f"{self.titulo} de {self.autor} ({self.ano}) - {'Disponível' if self.disponivel else 'Indisponível'}"
